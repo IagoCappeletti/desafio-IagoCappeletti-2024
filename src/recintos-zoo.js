0ed7f693d20a7648743a631d8cca935c76aaae86
IagoCappeletti <iago_cappeletti@hotmail.com>
2024-09-12 11:46:09 -0300

    refactore: adding exception handling

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,18 +1,8 @@
-import RecintoManager from "./recinto.manager.js";
-
 class RecintosZoo {
 
-        recintoManager = new RecintoManager()
+    analisaRecintos(animal, quantidade) {
+    }
 
-        analisaRecintos(animal, quantidade) {
-
-                try {
-                        const response = this.recintoManager.analisaRecintos(animal, quantidade)
-                        return { recintosViaveis: response }
-                } catch (erro) {
-                        return { erro: erro.message }
-                }
-        }
 }
 
 export { RecintosZoo as RecintosZoo };
